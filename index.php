3bb36f8d405a032f298a2cae7c7a095a6d71c232
MedicArwen <45285123+MedicArwen@users.noreply.github.com>
2021-07-09 10:56:44 +0200

    ajout des fichiers
    ajout

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+   <head>
+      <title>Bienvenue dans les examples PHP</title>
+   </head>
+   <body>
+    <?php
+    echo ('Bonjour ');
+    echo ('Bienvenue sur mon site');
+    ?>
+     <a href="tableaux.php"> exemple de tableaux</a><br>
+    <a href="form.php"> exemple de formulaire</a><br>
+    <a href="fichier_appel_inclure.php">exemple d'include</a><br>
+    <a href="passage_valeur.php">exemple de fonction</a><br>
+   </body>
+</html>
